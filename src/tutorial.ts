972470a34472b2a3556bbfba58d6405845456242
astrobemo <astrobemo@gmail.com>
2025-02-03 23:53:08 +0700

    add proccessData function

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,1 +1,8 @@
-console
+function processData(input: string | number, config: {reverse_property : boolean} ){
+    if(typeof input === 'string'){
+        return input.split('').reverse().join('');
+    }
+    if(typeof input === 'number'){
+        return input.toString().split('').reverse().join('');
+    }
+}
